bd7180f8599d175f5605ad48712e413e2cebba54
duhongwei <duhongwei@360.cn>
2021-06-26 20:26:09 +0800

    0.1.0

--- index.js
+++ index.js
@@ -1,41 +1,120 @@
-const babel = require.resolve('@babel/core')
-const preset = {
-  "@babel/preset-es2015": require.resolve('@babel/preset-es2015'),
-  "@babel/preset-react": require.resolve('@babel/preset-react')
-}
-//后续事先define需要的方法，然后再启用transformRuntime
-//const transformRuntime = require.resolve('@babel/plugin-transform-runtime')
-module.exports = function (opts = {}) {
-  return function (files, metalsmith, done) {
-    for (let file in files) {
-      if (!/\.js$/.test(file)) {
-        continue
+import babel from "@babel/core";
+import { join, dirname } from 'path'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+export default async function ({ debug, opt: { transforms = {} } }) {
+  let { util: { isHtml, isJs }, config: { logger } } = this
+  this.on('afterParse', (files) => {
+    const opts = {
+      transforms: Object.assign({ modules: false }, transforms)
+    }
+    for (let file of files) {
+      if (!isJs(file.key)) continue
+      if (/\.min\.js$/.test(file.key)) continue
+      if (/^(other|node)\/$/.test(file.key)) continue
+      if (file.meta && file.meta.isMin) continue
+
+      debug(`buble ${file.key}`)
+      try {
+        let code = babel.transformSync(file.content, {
+          "presets": [
+            [
+              "@babel/env",
+              {
+                "targets": {
+                  "chrome": "32",
+                },
+                "useBuiltIns":false
+                //"useBuiltIns": 'usage',
+                //'corejs':'3.6.5'
+              }
+            ]
+          ]
+        }).code;
+
+        code = code.replace(/function _classCallCheck.+/, '')
+        code = code.replace(/function _defineProperties.+/, '')
+        code = code.replace(/function asyncGeneratorStep.+/, '')
+        code = code.replace(/function _asyncToGenerator.+/, '')
+        code = code.replace(/function _createForOfIteratorHelper.+/, '')
+        code = code.replace(/function _unsupportedIterableToArray.+/, '')
+        code = code.replace(/function _arrayLikeToArray.+/, '')
+        file.content = code
+
       }
-
-      const thisOpts = Object.assign({}, {
-        filename: file,
-        ast: false,
-        presets: ["@babel/preset-es2015"],
-        plugins: [
-         /*  [
-            transformRuntime, {
-              "helpers": true,
-              "polyfill": true,
-            }
-          ] */
-        ]
-      }, opts);
-
-      thisOpts.presets = thisOpts.presets.map(item => preset[item])
-      const res = require(babel).transform(files[file].contents, thisOpts);
-      if (res !== null) {
-
-        if (!res.ignored) {
-          console.log(res.code)
-          files[file].contents = res.code;
-        }
+      catch (error) {
+        console.log(file.content)
+        logger.error(`error when compile ${file.key}\n ${error.message}`, true)
       }
     }
-    done()
-  }
+  })
+
+  this.on('afterKey', async function () {
+    let path = join(__filename, '../corejs.min.js')
+
+    let content = await this.fs.readFile(path)
+
+    this.files.push({
+      meta: { isMin: true },
+      key: 'node/corejs.min.js',
+      path,
+      content
+    })
+  })
+  this.on('afterGroup', function (files) {
+
+    debug('babel on event beforeDep')
+    let key1 = 'node/corejs.min.js'
+    let key2 = 'node/regenerator-runtime.js'
+
+    for (let file of files) {
+      if (!isHtml(file.key)) continue;
+
+      file.dep.jsList[0].unshift(key1)
+      file.dep.jsList[0].unshift(key2)
+    }
+  })
+  this.on('afterRead', async function () {
+    let prePath = dirname(require.resolve('regenerator-runtime'))
+    let path = ''
+
+    path = join(prePath, 'runtime.js')
+
+    let content = await this.fs.readFile(path)
+    content = `
+    ${content}
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+    function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+    function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+    function _createForOfIteratorHelper(o, allowArrayLike) {
+      var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() { }; return { s: F, n: function n() { if (i >= o.length) return { done: true };
+      return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance. In order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+      var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } };
+    }
+    function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+    function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+    window._classCallCheck=_classCallCheck
+    window._defineProperties=_defineProperties
+    window.asyncGeneratorStep=asyncGeneratorStep
+    window._asyncToGenerator=_asyncToGenerator
+    
+    window._createForOfIteratorHelper=_createForOfIteratorHelper
+    window._unsupportedIterableToArray=_unsupportedIterableToArray
+    window._arrayLikeToArray=_arrayLikeToArray
+    `
+    this.files.push({
+      //无论是dev,还是pro都标识为 min，就是不压缩,不转换
+      meta: { isMin: true },
+      path,
+      content
+    })
+  })
 }
+
